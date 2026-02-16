877945684f06bec35d488bf127a48643156040df
ajay kumar singh <as0317006@gmail.com>
2026-02-16 16:13:59 +0530

    Fix deployment errors: added nltk dependency and handled stopwords download safely

--- src/preprocess.py
+++ src/preprocess.py
@@ -1,16 +1,24 @@
 import re
-from nltk.corpus import stopwords
-from nltk.tokenize import word_tokenize
+import nltk
+
+# stopwords
+try:
+    from nltk.corpus import stopwords
+    stopwords.words("english")
+except LookupError:
+    nltk.download("stopwords")
+    from nltk.corpus import stopwords
 
 stop_words = set(stopwords.words("english"))
 
 def clean_text(text):
-    # Tokenize the text
     text = text.lower()
-    text  = re.sub(r'[^\w\s]', ' ', text)  # Remove punctuation
+    text = re.sub(r'[^\w\s]', ' ', text)  # Remove punctuation
     words = text.split()
+
     filtered_words = []
     for word in words:
         if word not in stop_words:
             filtered_words.append(word)
+
     return " ".join(filtered_words)
